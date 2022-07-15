24d21454a138b0092cddaed8e0223ce91b612376
whicencer <denielaz2020@gmail.com>
2022-07-15 15:15:21 +0300

    refactor: 05_is_array_int refactor

--- src/05_is_array_int.js
+++ src/05_is_array_int.js
@@ -1,1 +1,6 @@
-isIntArray = arr => arr && arr.every(el => Number.isInteger(el)) && arr.every(el => !isNaN(el));
+function isIntArray(arr) {
+  if(arr && arr.every(el => Number.isInteger(el)) && arr.every(el => !isNaN(el))) {
+    return true
+  }
+  return false
+}
